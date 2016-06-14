110b6b8b5185a70abd225542d01094a54bbe7eda
Andrew Easter <aeaster@gumtree.com>
2016-06-14 21:44:39 +0100

    further prototyping

--- src/main/java/com/dreweaster/ddd/framework/EventStore.java
+++ src/main/java/com/dreweaster/ddd/framework/EventStore.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public interface EventStore {
 
-    <A, E> List<PersistedEvent<A, E>> loadEvents(Class<A> aggregateType, AggregateId aggregateId);
+    <A extends Aggregate<E, ?>, E extends DomainEvent> List<PersistedEvent<A, E>> loadEvents(
+            Class<A> aggregateType,
+            AggregateId aggregateId);
 
-    <A, E> List<PersistedEvent<A, E>> saveEvents(
+    <A extends Aggregate<E, ?>, E extends DomainEvent> List<PersistedEvent<A, E>> saveEvents(
             Class<A> aggregateType,
             AggregateId aggregateId,
             CommandId commandId,
